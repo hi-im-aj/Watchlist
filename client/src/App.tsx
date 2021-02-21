2ab015901819b43e4ee6cae4bab93f3a209b250a
Anders Højer Jørgensen <virtualzeth@gmail.com>
2021-02-21 14:26:19 +0100

    Bootstrap and basic Navbar setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
 const App = () => {
   return (
     <Fragment>
-      <Navbar />
-      <main className="App"></main>
+      <Router>
+        <Navbar />
+        <main className="App">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="*" component={NotFound} />
+          </Switch>
+        </main>
+      </Router>
     </Fragment>
   );
 };
